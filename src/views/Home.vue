7acfc147aa05550c953af6d99ca2b791671dbc6a
Cade Daniel <edacih@gmail.com>
2019-04-16 20:19:19 -0700

    veutify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,13 @@
 <template>
-<div class="home" v-if="user">
-  <image-gallery :photos="photos" />
-</div>
-<div v-else>
-  <p>If you would like to view photos, please register for an account or login.</p>
-  <router-link to="/register" class="pure-button">Register</router-link> or
-  <router-link to="/login" class="pure-button">Login</router-link>
-</div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import ImageGallery from '@/components/ImageGallery.vue'
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'home',
-  components: {
-    ImageGallery
-  },
-  computed: {
-    user() {
-      return this.$store.state.user;
-    },
-    photos() {
-      return this.$store.state.photos;
+  export default {
+    components: {
+      HelloWorld
     }
-  },
-  async created() {
-    await this.$store.dispatch("getUser");
-    await this.$store.dispatch("getAllPhotos");
-  },
-}
+  }
 </script>
