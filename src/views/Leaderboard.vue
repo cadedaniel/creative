62aacedce60abd0c05758b742a8345c49fe159d3
Cade Daniel <edacih@gmail.com>
2019-04-17 18:12:59 -0700

    Mock leaderboard

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,23 +1,67 @@
 <template>
     <div>
-      hello there
+        <h2>StarCraft Leaderboard</h2>
+        <v-data-table :headers="headers" :items="ranks" class="elevation-1">
+            <template v-slot:items="props">
+                <td>{{ props.item.rank }}</td>
+                <td class="text-xs-right">{{ props.item.id}}</td>
+                <td class="text-xs-right">{{ props.item.country}}</td>
+                <td class="text-xs-right">{{ props.item.mmr }}</td>
+                <td class="text-xs-right">{{ props.item.main_race }}</td>
+            </template>
+        </v-data-table>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'leaderboard',
-  components: {
-  },
-  data() {
-    return {
+    name: 'leaderboard',
+    components: {
+    },
+    data () {
+        return {
+            headers: [
+            {
+                text: 'Rank',
+                align: 'left',
+                sortable: true,
+                value: 'rank'
+            },
+            { text: 'ID', value: 'id' },
+            { text: 'Country', value: 'country' },
+            { text: 'MMR', value: 'mmr' },
+            { text: 'Main Race', value: 'main_race' },
+            ],
+            ranks: [
+            {
+                rank: '1',
+                id: 'FlaShGGre',
+                country: 'Korea',
+                mmr: '2954',
+                main_race: 'Terran',
+            },
+            {
+                rank: '2',
+                id: 'SKT_Larva',
+                country: 'Korea',
+                mmr: '2633',
+                main_race: 'Zerg',
+            },
+            {
+                rank: '3',
+                id: 'rain_arena',
+                country: 'Korea',
+                mmr: '2591',
+                main_race: 'Protoss',
+            },
+            ]
+        }
+    },
+    computed: {
+    },
+    async created() {
+    },
+    methods: {
     }
-  },
-  computed: {
-  },
-  async created() {
-  },
-  methods: {
-  }
 }
 </script>
